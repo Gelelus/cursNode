1c0822b9ec9bac831996d122861ab3ddf69ea37a
Uri <47351041+Gelelus@users.noreply.github.com>
2020-10-01 14:47:09 +0300

    Update option.js

--- task1/option.js
+++ task1/option.js
@@ -2,8 +2,8 @@ module.exports = function setOption(program) {
   return program
     .storeOptionsAsProperties(false)
     .passCommandToAction(false)
-    .requiredOption('-s, --shift <action>', 'a shift')
-    .option('-i, --input <action>', 'an input file')
-    .option('-o, --output <action>', 'an output file')
+    .requiredOption('-s, --shift <number>', 'a shift')
+    .option('-i, --input <path>', 'an input file')
+    .option('-o, --output <path>', 'an output file')
     .requiredOption('-a, --action <action>', 'an action encode/decode');
 };
